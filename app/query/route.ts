420c94f257846ce1c3ac9fd01b7d745ab41bbe3c
M4ttH3W711 <matthew711.uni@gmail.com>
2025-07-23 10:11:57 -0600

    responseJsonDeleted

--- app/query/route.ts
+++ app/query/route.ts
@@ -8,19 +8,7 @@ async function listInvoices() {
     FROM invoices
     JOIN customers ON invoices.customer_id = customers.id
     WHERE invoices.amount = 666;
-`;
+  `;
 
   return data;
 }
-
-//export async function GET() {
-//  return Response.json({
-//    message:
-//      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-//  });
-  // try {
-  // 	return Response.json(await listInvoices());
-  // } catch (error) {
-  // 	return Response.json({ error }, { status: 500 });
-  // }
-//}
